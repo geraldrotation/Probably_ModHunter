6a8431d66b6a9fa096e653ebd81a967ea700962c
modhunter <pemodhunter@gmail.com>
2014-01-10 15:36:23 -0500

    more pausing scenarios prevent tranq shot & serpent sting if the target is charmed

--- survival.lua
+++ survival.lua
@@ -18,8 +18,17 @@ ProbablyEngine.rotation.register_custom(255, "ModHunter_SV",
 
    { "109248" , "modifier.lcontrol", "ground" }, -- Binding Shot
 
+   -- Pre DPS Pause
+   { "pause", "target.debuff(Wyvern Sting).any" },
+   { "pause", "target.debuff(Scatter Shot).any" },
+   { "pause", "target.immune.all" },
+   { "pause", "target.status.disorient" },
+   { "pause", "target.status.incapacitate" },
+   { "pause", "target.status.sleep" },
+
+
    -- Serpent Sting on mouseover when they don't have the debuff already and the toggle is enabled
-   { "1978", { "!mouseover.debuff(118253)", "toggle.autoSS" }, "mouseover" },
+   { "1979", { "!mouseover.debuff(118253)", "toggle.autoSS", "mouseover.exists", "mouseover.enemy", "mouseover.alive", "!mouseover.state.charm" }, "mouseover" },
 
    -- Interrupt(s)
    { "147362", "target.interruptAt(50)" }, -- Counter Shot at 50% cast time left
@@ -65,7 +74,7 @@ ProbablyEngine.rotation.register_custom(255, "ModHunter_SV",
    { "3045" }, -- Rapid Fire
    { "120679" }, -- Dire Beast
    { "82726", "player.focus < 50" }, -- Fervor when under 50 focus
-   { "19801", "target.dispellable(19801)", "target" }, -- Tranquilizing Shot
+   { "19801", { "target.dispellable(19801)", "!target.state.charm" }, "target" }, -- Tranquilizing Shot
 
    -- AoE
    {{
@@ -76,7 +85,7 @@ ProbablyEngine.rotation.register_custom(255, "ModHunter_SV",
    }, { "modifier.multitarget", "modifier.enemies >= 3" }, },
 
    -- Single
-   { "1978", "!target.debuff(118253)" }, -- Serpent Sting if SS debuff is not present
+   { "1978", { "!target.debuff(118253)", "target.ttd >= 10", "!target.state.charm" }}, -- Serpent Sting if SS debuff is not present
    { "3674", { "!target.debuff(3674)", "target.ttd >= 8"  }}, -- Black Arrow
    { "77767", "target.debuff(118253).duration < 4" }, -- Cobra Shot if SS duration < 4 secs
    { "53301" }, -- Explosive Shot
@@ -88,6 +97,8 @@ ProbablyEngine.rotation.register_custom(255, "ModHunter_SV",
 },
 {
   -- Out of combat
+   { "pause", "modifier.lshift" },
+   { "pause","player.buff(5384)" }, -- Pause for Feign Death
    { "136", { "pet.health <= 90", "pet.exists", "!pet.dead", "!pet.buff(136)" }}, -- Mend Pet
    {{
       { "5118", { "player.moving", "!player.buff(5118)" }}, -- Cheetah
